060ce81ed878bf8a0fc3a723c3ee0ffad3524695
t-cano <macbookairm1@T-cano.local>
2024-08-15 19:41:38 +0300

    kodlar duzenlendi

--- 1st/ilkask.py
+++ 1st/ilkask.py
@@ -2,28 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Student_performance_data _.csv')
-from sklearn.linear_model import LinearRegression
-X = dataset['Absences'].values
-y= dataset['GPA'].values
 
-#when we used one column that "[:, None]" part necessary for arry format
-#If use more than one column just delete it 
-X = X[:, None]
-print(X)
+df = pd.read_csv('/content/Student_performance_data _.csv')
 
+# try to make absences column to Attendance so firstly find max value absent 
+maxValue = df['Absences'].max()
+
+# after that subtract all rows from max number
+for index, row in df.iterrows():
+    df.at[index, 'Absences'] =  29 - row['Absences'] 
+#rename Absences
+df = df.rename(columns={'Absences': 'Attendance'})
+
+
+X = df[['Attendance']].values
+y= df['GPA'].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
 
-#Create our model
+#build model for correlation line
 from sklearn.linear_model import LinearRegression
+
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
 
-#let's sketch the graph
+#sketch the graph
 plt.scatter(X_train, y_train, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('derse girmeme vs GPA (Training set)')
-plt.xlabel('ABSENT')
+plt.title('Attendance / GPA Correlation')
+plt.xlabel('Absences')
 plt.ylabel('GPA')
 plt.show()
